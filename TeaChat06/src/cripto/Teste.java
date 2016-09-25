49b8c70c90ccd8aced136e0853186e844d4e0bfc
EABraga <elciobraga10@gmail.com>
2016-09-25 16:22:59 -0300

    Última versão 6.01 com os BUGs corrigidos.

---
+++ TeaChat06/src/cripto/Teste.java
@@ -0,0 +1,34 @@
+package cripto;
+
+import java.security.InvalidKeyException; 
+import java.security.NoSuchAlgorithmException; 
+
+import javax.crypto.BadPaddingException; 
+import javax.crypto.IllegalBlockSizeException; 
+import javax.crypto.NoSuchPaddingException; 
+
+public  class  Teste {
+	
+
+    public static void main(String[] args) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
+
+    	EncriptaDecriptaDES ed;
+    	String sTxt = "Qual será a fase lunar de hoje?";
+    	byte[] yCrp;
+    			
+    	System.out.println("Exemplo de uso do sistema de Criptografia");
+    	System.out.println("-----------------------------------------");
+    	System.out.println("");
+    	System.out.println("Texto Puro: "+sTxt);
+    	
+    	ed = new EncriptaDecriptaDES();
+    	ed.setCripty(sTxt, "", "");
+    	yCrp = ed.getCript();
+    	
+    	System.out.println("Texto Criptografado (com senha padrão): "+yCrp.toString());
+    	//System.out.println(yCrp);
+    	
+    }
+
+
+}
